5a382d6c2b6efcadb5622e42340d4150e4348c3e
Heli Türi <heli.turi@voco.ee>
2023-03-31 21:48:24 +0300

    Get books from localStorage

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 const form = document.querySelector('#book-form')
 form.addEventListener('submit', addBook)
 
-function addBook(event){
-    // read user inputs
-    const title = document.querySelector('#title').value
-    const author = document.querySelector('#author').value
-    const isbn = document.querySelector('#isbn').value
+document.addEventListener('DOMContentLoaded', getBooks)
 
-    // create table row with user inputs data
-    const bookRow = document.createElement('tr')
-    bookRow.innerHTML = `
-                        <td>${title}</td>
-                        <td>${author}</td>   
-                        <td>${isbn}</td>   
-                        <td><a href="#">X</a></td>`
-    const booksTable = document.querySelector('#books')
-    booksTable.appendChild(bookRow)
-
-    // add book to LS
-    const book = [title, author, isbn]
-    let books // array for user inputs
+function getBooks(){
+    let books // array for LS data
     if(localStorage.getItem('books') === null){
         books = []
     } else {
         books = JSON.parse(localStorage.getItem('books'))
     }
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
-
-    // clear inputs
-    document.querySelector('#title').value = ''
-    document.querySelector('#author').value = ''
-    document.querySelector('#isbn').value = ''
-    event.preventDefault()
+    books.forEach(function(book){
+        const bookRow = document.createElement('tr')
+        bookRow.innerHTML = `
+                        <td>${book[0]}</td>
+                        <td>${book[1]}</td>   
+                        <td>${book[2]}</td>   
+                        <td><a href="#">X</a></td>`
+        const booksTable = document.querySelector('#books')
+        booksTable.appendChild(bookRow)
+    })
 }
+
+function addBook(event){
+    // read user inputs
+    const title = document.querySelector('#title').value
